bb4fff0544d246541188705f490eb84c3069d004
Roxane Belahcene <78685086+Roxyroke@users.noreply.github.com>
2023-03-31 15:07:29 +0200

    Update App.jsx
    react basic 4

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,22 @@ import './App.css'
 
 
 function App() {
- 
+
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+const pokemon = pokemonList[0]
 
   return (
-<div>
- <PokemonCard />
+  <div>
+ <PokemonCard pokemon = {pokemon.name}/>
 </div>
   )
 }
