24e8c3d4c2a238254899f200cac8ca50572c651a
Roxane Belahcene <78685086+Roxyroke@users.noreply.github.com>
2023-03-31 15:11:48 +0200

    Update PokemonCard.jsx
    react basic 4

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,23 +1,12 @@
- function PokemonCard() {
- 
-   const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+ function PokemonCard({pokemon}) {
 
-   const pokemon = pokemonList[1]
-  return (
-    <figure>
-    {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name}/>):(<p>???</p>)}
+ return 
+<div>
+  <figure>
+    {pokemon.name ==="bulbasaur" ?<img src={pokemon.imgSrc} alt={pokemon.name}/>:(<p>???</p>)},
     <figcaption>{pokemon.name}</figcaption>
-        </figure>
-    );
-  }
+ </figure>
+ </div>
+  };
 
 export default PokemonCard;
