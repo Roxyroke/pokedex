1961d1f6deac5e0b1ba8a7253ac04d31748f2c6c
Roxane Belahcene <roxounette@aol.com>
2023-03-31 15:02:32 +0200

    react basic 4

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import PokemonCard from './components/PokemonCard.jsx'
 import './App.css'
 
-function App() {
-  return (
-    <div>
- <PokemonCard />
-    </div>
 
+function App() {
+ 
+
+  return (
+<div>
+ <PokemonCard />
+</div>
   )
 }
 
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,23 @@
  function PokemonCard() {
-    return (
-  
-   <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-             alt="PokemonCard"/>
-             <figcaption>bulbasur</figcaption>
-    </figure>
+ 
+   const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
-    )
+   const pokemon = pokemonList[1]
+  return (
+    <figure>
+    {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name}/>):(<p>???</p>)}
+    <figcaption>{pokemon.name}</figcaption>
+        </figure>
+    );
   }
-  
+
 export default PokemonCard;
