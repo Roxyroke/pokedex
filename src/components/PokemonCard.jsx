b032440ac1d2ee164983d4bcdcd66fa48ea24b43
Roxane Belahcene <78685086+Roxyroke@users.noreply.github.com>
2023-04-01 17:15:14 +0200

    Update PokemonCard.jsx
    react basic 7

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -3,19 +3,19 @@ import PropTypes from "prop-types";
 
 function PokemonCard({ pokemon }) {
 
-    return <div>
+    return (
         <figure>
             {pokemon.name === "bulbasaur" ? <img src={pokemon.imgSrc} /> : <p>???</p>}
             <figcaption>{pokemon.name}</figcaption>
         </figure>
-    </div>;
+);
 }
 
 PokemonCard.propTypes = {
     pokemon: PropTypes.shape({
         name: PropTypes.string.isRequired,
         imgSrc: PropTypes.string.isRequiered,
-    })
+    }).isRequired,
 }
 
 export default PokemonCard;
