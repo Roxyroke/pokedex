665a6cd95e24832060011a14f0473c945a5ee40f
Roxane Belahcene <78685086+Roxyroke@users.noreply.github.com>
2023-04-01 13:16:03 +0200

    Update App.jsx
    react basic 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,41 @@
-//import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-import PokemonCard from './components/PokemonCard.jsx'
-import './App.css'
-
+import "./App.css";
+import PokemonCard from "./components/PokemonCard"; 
+import React, { useState } from "react";
 
 function App() {
-
   const pokemonList = [
     {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-const pokemon = pokemonList[0]
-
+        name: "bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "charmander",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      },
+      {
+        name: "squirtle",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      },
+      {
+        name: "pikachu",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      },
+      {
+        name: "mew",
+      },
+    ];
+  const pokemon = pokemonList[0];
   return (
-  <div>
- <PokemonCard pokemon = {pokemon.name}/>
-</div>
-  )
+    <div>
+      <PokemonCard pokemon={pokemon} />
+    </div>
+  );
 }
 
-export default App
+
+
+export default App;
