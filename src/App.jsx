c8c33e0638945963fc1f732e97f34f44b594178d
Roxane Belahcene <78685086+Roxyroke@users.noreply.github.com>
2023-04-01 13:23:06 +0200

    Update App.jsx
     react basic 06

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,55 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard"; 
-import React, { useState } from "react";
+import { useState } from "react";
 
 function App() {
-  const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-      },
-      {
-        name: "charmander",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-      },
-      {
-        name: "squirtle",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-      },
-      {
-        name: "pikachu",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-      },
-      {
-        name: "mew",
-      },
-    ];
-  const pokemon = pokemonList[0];
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  const handleClick1 = () => {
+    setPokemonIndex(pokemonIndex - 1)
+  }
+
+  const handleClick2 = () => {
+    setPokemonIndex(pokemonIndex + 1)
+  }
+
   return (
     <div>
-      <PokemonCard pokemon={pokemon} />
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      <button onClick={handleClick1}>Précedent</button>
+      <button onClick={handleClick2}>Suivant</button>
     </div>
   );
 }
 
 
+const pokemonList = [
+  {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 export default App;
+
+
+
+
