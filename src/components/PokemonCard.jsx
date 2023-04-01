34fec70342398846f2f3b8a3595ef5075d2ccbf9
Roxane Belahcene <78685086+Roxyroke@users.noreply.github.com>
2023-04-01 12:51:34 +0200

    Update PokemonCard.jsx
    enregistrer compromets.2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,29 @@
- function PokemonCard() {
-    return (
-  
-   <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-             alt="PokemonCard"/>
-             <figcaption>bulbasur</figcaption>
-    </figure>
+function PokemonCard() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
-    )
-  }
-  
+  const pokemon = pokemonList[1];
+
+  return (
+    <div>
+      <figure>
+        {pokemon.name === "bulbasaur" ? (
+          <img src={pokemon.imgSrc} />
+        ) : (
+          <p>???</p>
+        )}
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    </div>
+  );
+}
+
 export default PokemonCard;
