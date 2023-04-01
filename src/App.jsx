436a38cd6fe7df963b12c45fef12e0e4c86f5ec2
Roxane Belahcene <78685086+Roxyroke@users.noreply.github.com>
2023-04-01 12:52:51 +0200

    Update App.jsx
    enregistrer basic react 03

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import PokemonCard from './components/PokemonCard.jsx'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import MyTitle from "./components/MyTitle"
 import './App.css'
+import PokemonCard from "./components/PokemonCard"
+
 
 function App() {
-  return (
-    <div>
- <PokemonCard />
-    </div>
 
+  return (
+    <div className="App">
+      <PokemonCard />
+    </div>
   )
 }
 
