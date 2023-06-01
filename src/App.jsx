4c294c9df8c37854170dc8fffc05a0735d2c257d
Roxane Belahcene <78685086+Roxyroke@users.noreply.github.com>
2023-06-01 10:41:44 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -39,8 +39,8 @@ const pokemonList = [
     //pokemonList.map(Element => (<PokemonCard name ={Element}/>))
    <div> 
     <PokemonCard pokemon={pokemonList[pokemonIndex]}/> 
-    { pokemonIndex > 0  ? <button onClick={handleClickPrev}>Précédent</button> : ""}
-    { pokemonIndex < pokemonList.length - 1 ? <button onClick={handleClickNext}>Suivant</button> : ""}
+   //{ pokemonIndex > 0  ? <button onClick={handleClickPrev}>Précédent</button> : ""}
+    //{ pokemonIndex < pokemonList.length - 1 ? <button onClick={handleClickNext}>Suivant</button> : ""}
      <Navbar pokemonIndex={pokemonIndex} setpokemonIndex={setpokemonIndex} pokemonList={pokemonList}/>
     </div>
   );
